0353e931d228a8319b06d86beff8fc407e77bc7a
Christy <tattooclarity@gmail.com>
2026-03-03 19:22:35 -0800

    fix outputFileTracingIncludes

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    outputFileTracingIncludes: {
-      "/api/download": ["./storage/designs/**/*"],
-    },
+  outputFileTracingIncludes: {
+    "/api/download": ["./storage/designs/**/*"],
   },
-};
+} as any;
 
 export default nextConfig;
