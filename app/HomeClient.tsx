d5731f23488d6c9226c263bd1e2f689b88468e88
Christy <tattooclarity@gmail.com>
2026-03-05 12:32:25 -0800

    add facebook icon in footer

--- app/HomeClient.tsx
+++ app/HomeClient.tsx
@@ -2499,10 +2499,21 @@ export default function HomeClient() {
               <img src="/social/email.png" alt="Email Us" style={{ height: 22 }} />
             </a>
             
-            {/* ✅【新增】Facebook Icon 與連結 */}
-            <a href="https://www.facebook.com/tattooclarity" target="_blank" rel="noopener noreferrer" style={{ display: 'flex', alignItems: 'center' }}>
-              <img src="/social/facebook.png" alt="Follow us on Facebook" style={{ height: 22 }} />
-            </a>
+           {/* ✅ 使用 SVG 完美顯示 Facebook Icon，唔使讀取圖片檔，自帶 hover 變金色效果 */}
+<a href="https://www.facebook.com/tattooclarity" target="_blank" rel="noopener noreferrer" style={{ display: 'flex', alignItems: 'center' }}>
+  <svg 
+    width="22" 
+    height="22" 
+    viewBox="0 0 24 24" 
+    fill="#141414" 
+    xmlns="http://www.w3.org/2000/svg" 
+    style={{ transition: 'fill 0.2s', cursor: 'pointer' }} 
+    onMouseEnter={(e) => e.currentTarget.style.fill = 'var(--gold)'} 
+    onMouseLeave={(e) => e.currentTarget.style.fill = '#141414'}
+  >
+    <path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" />
+  </svg>
+</a>
 
             <div style={{ position: 'relative', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
               <img
